83410301a34f97c1303a18599470fafc6b23f4ba
Mike Kishline <mkishline@carthage.edu>
2015-01-22 15:23:37 -0600

    Initial skeleton for housing project

---
+++ Housing/Default_View.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Default_View.ascx.cs" Inherits="Housing.Default_View" %>
---
+++ Housing/HousingMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Jenzabar.Portal.Framework.Web;
+using Jenzabar.Portal.Framework.Web.UI;
+
+namespace Housing
+{
+    public class HousingMain : PortletBase
+    {
+        protected override PortletViewBase GetCurrentScreen()
+        {
+            PortletViewBase screen = null;
+            switch(this.CurrentPortletScreenName)
+            {
+                case "Main":
+                    screen = this.LoadPortletView("ICS/Portlet.Housing/Main_View.ascx");
+                    break;
+                case "Default"
+                default:
+                    screen = this.LoadPortletView("ICS/Portelt.Housing/Default_View.ascx");
+                    break;
+            }
+            return screen;
+            //throw new NotImplementedException();
+        }
+    }
+}
