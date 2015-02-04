c4e65d7194e9064ce50f6ef202dd59d4f86c1508
Mike Kishline <mkishline@carthage.edu>
2015-02-04 11:16:37 -0600

    Fixed query parameter issues

--- Housing/Default_View.ascx.cs
+++ Housing/Default_View.ascx.cs
@@ -18,11 +18,11 @@ namespace Housing
 
         public override string ViewName { get { return "Housing Sign-up Application"; } }
 
-        public string SpringSession { get { return "RA"; } }
-        public string FallSession { get { return "RC"; } }
+        public string SpringSession { get { return "RC"; } }
+        public string FallSession { get { return "RA"; } }
         //public string CurrentYear { get { return new DateTime().Year.ToString(); } }
         public string CurrentYear { get { return "2014"; } }
-        public string NextYear { get { return (CurrentYear + 1).ToString(); } }
+        public string NextYear { get { return (int.Parse(CurrentYear) + 1).ToString(); } }
         public string BeginInvolvement { get { return (int.Parse(CurrentYear) - 3).ToString() + "-01-01"; } }
         //public int UserID { get { return int.Parse(PortalUser.Current.HostID); } }
         public int UserID { get { return 1339128; } }
@@ -31,30 +31,32 @@ namespace Housing
         {
             if (this.IsFirstLoad)
             {
-                InitScreenStatic();
+                InitScreen();
             }
         }
 
-        protected void InitScreenStatic()
+        protected void InitScreen()
         {
-            string studentSQL = @"
+            string studentSQL = String.Format(@"
                     SELECT
-                        id_rec.id, TRIM(id_rec.firstname) AS firstname, TRIM(id_rec.lastname) AS lastname, profile_rec.sex, TRIM(aa_rec.line1) AS email, stu_stat_rec.cum_earn_hrs AS careerhours,
-                        TRIM(NVL(stu_serv_rec.bldg,'')) AS bldg, TRIM(NVL(stu_serv_rec.room,'')) AS room, stu_acad_rec.reg_hrs AS registered_hours, NVL(ADVpay.hld,'') AS advPayHold,
-                        NVL(UNBal.hld,'') AS unbalHold, involve_rec.invl AS greekid, invl_table.txt AS greekname, TRIM(upcoming.bldg) AS upcomingBldg, TRIM(upcoming.room) AS upcomingRoom
+                        id_rec.id, TRIM(id_rec.firstname) AS firstname, TRIM(id_rec.lastname) AS lastname, profile_rec.sex, TRIM(aa_rec.line1) AS email, NVL(stu_stat_rec.cum_earn_hrs, 0) AS career_hours,
+                        TRIM(NVL(curbldg.txt,'')) AS bldg, TRIM(NVL(stu_serv_rec.room,'')) AS room, NVL(stu_acad_rec.reg_hrs, 0) AS registered_hours, NVL(ADVpay.hld,'') AS advpayhold,
+                        NVL(UNBal.hld,'') AS unbal_hold, involve_rec.invl AS greekid, TRIM(involve_rec.org) AS greek_name, TRIM(NVL(newbldg.txt,'')) AS upcoming_bldg, TRIM(NVL(upcoming.room,'')) AS upcoming_room
                     FROM id_rec	INNER JOIN	profile_rec			        ON	id_rec.id			=	profile_rec.id
                                 LEFT JOIN	aa_rec				        ON	id_rec.id			=	aa_rec.id
 											                            AND	aa_rec.aa			=	'EML1'
                                 LEFT JOIN	stu_stat_rec		        ON	id_rec.id			=	stu_stat_rec.id
                                 LEFT JOIN	stu_serv_rec		        ON	id_rec.id			=	stu_serv_rec.id
 											                            AND	stu_serv_rec.sess	=	?
-											                            AND	stu_serv_rec.yr		=	YEAR(TODAY)
+											                            AND	stu_serv_rec.yr		=	{0}
+                                LEFT JOIN   bldg_table      curbldg     ON  stu_serv_rec.bldg   =   curbldg.bldg
 			                    LEFT JOIN	stu_serv_rec	upcoming	ON	id_rec.id	        =	upcoming.id
 											                            AND	upcoming.sess		=	?
-											                            AND	upcoming.yr			=	YEAR(TODAY)
+											                            AND	upcoming.yr			=	stu_serv_rec.yr
+                                LEFT JOIN   bldg_table      newbldg     ON  upcoming.bldg       =   newbldg.bldg
 			                    LEFT JOIN	stu_acad_rec		        ON	id_rec.id			=	stu_acad_rec.id
 											                            AND	stu_acad_rec.sess	=	?
-											                            AND	stu_acad_rec.yr		=	YEAR(TODAY)
+											                            AND	stu_acad_rec.yr		=	stu_serv_rec.yr
 			                    LEFT JOIN	hold_rec	    ADVpay	    ON	id_rec.id			=	ADVpay.id
 											                            AND	TODAY			BETWEEN	ADVpay.beg_date	AND	NVL(ADVpay.end_date, TODAY)
 											                            AND	ADVpay.hld			=	'APAY'
@@ -62,29 +64,26 @@ namespace Housing
 											                            AND	TODAY			BETWEEN	UNBal.beg_date	AND	NVL(UNBal.end_date, TODAY)
 											                            AND	UNBal.hld			=	'UBAL'
 			                    LEFT JOIN	(
-				                    SELECT		invl, id, MAX(beg_date) AS beg_date, MAX(end_date) AS end_date
+				                    SELECT		invl, org, id, MAX(beg_date) AS beg_date, MAX(end_date) AS end_date
 				                    FROM		involve_rec
-				                    WHERE		id = ?
-				                    GROUP BY	invl, id
+				                    WHERE		id = {1}
+				                    GROUP BY	invl, org, id
 			                    )			involve_rec			ON	id_rec.id			=	involve_rec.id
 											                    AND
 											                    (
 												                    NVL(involve_rec.invl,'')	IN	('','S007','S025','S045','S061','S063','S092','S141','S152','S165','S168','S189','S190','S192','S194')
 											                    )
-											                    AND	NVL(involve_rec.beg_date, ?)	>=	?
-			                    LEFT JOIN	invl_table			ON	involve_rec.invl	=	invl_table.invl
-                    WHERE id_rec.id	=	?
-                ";
+											                    AND	NVL(involve_rec.beg_date, '{2}')	>=	?
+                                                                AND NVL(involve_rec.end_date, TODAY)    >=  TODAY
+                    WHERE id_rec.id	=	{3}
+                ", CurrentYear, UserID, BeginInvolvement, UserID);
 
             List<OdbcParameter> parameters = new List<OdbcParameter>
                 {
-                    new OdbcParameter("SpringSess", SpringSession),
-                    new OdbcParameter("FallSess", FallSession),
-                    new OdbcParameter("FallSess2", FallSession),
-                    new OdbcParameter("ID", UserID),
-                    new OdbcParameter("InvolveStartDate", BeginInvolvement),
-                    new OdbcParameter("InvolveStartDate2", BeginInvolvement),
-                    new OdbcParameter("ID2", UserID)
+                      new OdbcParameter("SpringSess", SpringSession)
+                    , new OdbcParameter("FallSess", FallSession)
+                    , new OdbcParameter("FallSess2", FallSession)
+                    , new OdbcParameter("InvolveStartDate", BeginInvolvement)
                 };
             Exception ex = null;
             DataTable dt = null;
@@ -101,12 +100,13 @@ namespace Housing
             {
                 //If an exception was thrown, display the message to the user.
                 //TODO: Consider turning this off unless the user is an administrator. Alternatively, throw custom exceptions where possible with more readable errors.
-                this.ParentPortlet.ShowFeedback(FeedbackType.Error, ee.Message);
+                this.ParentPortlet.ShowFeedback(FeedbackType.Error, String.Format("{0}<br /><br />{1}", ee.Message, ee.InnerException.ToString()));
             }
             finally
             {
                 //Regardless of whether the data loaded correctly or an error occurred, if the connection is still open, close it.
                 if (odbcConn.IsNotClosed()) { odbcConn.Close(); }
+                this.errMsg.ErrorMessage = studentSQL;
             }
 
             //If the query returned results, load the data.
@@ -114,37 +114,59 @@ namespace Housing
             {
                 DataRow dr = dt.Rows[0];
                 
-                this.ltlStudentName.Text = String.Format("{0} {1}", dt.Rows[0]["firstname"].ToString(), dt.Rows[0]["lastname"].ToString());
+                this.ltlStudentName.Text = String.Format("{0} {1}", dr["firstname"].ToString(), dr["lastname"].ToString());
 
-                this.panelRegistered.Visible = this.ltlRegisteredHousing.Text.Length > 0;
-                this.ltlRegisteredHousing.Text = "Denhart 120";
+                this.ltlRegisteredHousing.Text = String.Format("{0} {1}", dr["upcoming_bldg"].ToString(), dr["upcoming_room"].ToString()).Trim();
+                //this.panelRegistered.Visible = this.ltlRegisteredHousing.Text.Length > 0;
 
                 DateTime registeredDateTime = DateTime.Now;
                 this.ltlRegisteredDateTime.Text = String.Format("{0:hh:mm:ss tt on dddd MMMM d, yyyy}", registeredDateTime);
                 //this.bulletedRoommates - load roommate list
 
-                this.panelCommuter.Visible = this.ltlRegisteredHousing.Text == "CMTR";
+                //this.panelCommuter.Visible = this.ltlRegisteredHousing.Text == "CMTR";
 
-                this.panelUnregistered.Visible = this.ltlRegisteredHousing.Text.Length == 0;
+                //this.panelUnregistered.Visible = this.ltlRegisteredHousing.Text.Length == 0;
 
-                this.ltlGreekStatus.Text = "a member of [GreekOrgName]"; //"not a member of a residential fraternity or sorority"
+                //this.ltlGreekStatus.Text = dr["greekname"].ToString().Length > 0 ? String.Format("a member of {0}", dr["greekname"].ToString()) : "not a member of a residential fraternity or sorority";
+                this.ltlGreekStatus.Text = dr["greek_name"].ToString();
                 this.ltlHold.Text = ""; //"not"
-                //this.ltlHoldDetail.Visible = this.ltlHold.Text.Length > 0;
-                this.contentHoldDetail.Visible = this.ltlHold.Text.Length > 0;
-                this.ltlRegistered.Text = ""; //"not"
-                //this.panelRegisteredDetail.Visible = this.ltlRegistered.Text.Length > 0;
-                this.contentRegisteredDetail.Visible = this.ltlRegistered.Text.Length > 0;
+                this.ltlHold.Text = String.Format("{0} {1}", dr["advPayHold"].ToString(), dr["unbal_hold"].ToString()).Trim();
+                //this.contentHoldDetail.Visible = this.ltlHold.Text.Length > 0;
+                //A student must be registered for 12 or more credits in the upcoming semester
+                this.ltlRegistered.Text = int.Parse(dr["registered_hours"].ToString()) >= 12 ? "" : "not";
+                
+                //this.contentRegisteredDetail.Visible = this.ltlRegistered.Text.Length > 0;
+                
                 this.ltlRegisteredYear.Text = String.Format("{0} - {1}", CurrentYear, NextYear);
-                this.ltlGender.Text = "You are male"; //"female", "Our records don't indicate your gender"
-                this.ltlCareerCredits.Text = "120";
-                this.ltlCurrentHousing.Text = "Denhart 119";
-                this.panelCurrentHousing.Visible = this.ltlCurrentHousing.Text.Length > 0;
-                this.ltlNotResident.Visible = this.ltlCurrentHousing.Text.Length == 0;
+                //Determine the proper display of student's gender
+                switch (dr["sex"].ToString())
+                {
+                    case "M":
+                        this.ltlGender.Text = "You are male";
+                        break;
+                    case "F":
+                        this.ltlGender.Text = "You are female";
+                        break;
+                    default:
+                        this.ltlGender.Text = "Our records don't indicate your gender";
+                        break;
+                }
+                //Display the total number of credit hours earned by the student over their academic career
+                this.ltlCareerCredits.Text = dr["career_hours"].ToString();
+
+                this.ltlCurrentHousing.Text = String.Format("{0} {1}", dr["bldg"].ToString(), dr["room"].ToString());
+                //this.panelCurrentHousing.Visible = this.ltlCurrentHousing.Text.Length > 0;
+                //this.ltlNotResident.Visible = this.ltlCurrentHousing.Text.Length == 0;
 
                 bool isValidTime = true, mayRegister = true;
-                this.lnkAvailability.Visible = isValidTime && mayRegister;
-                this.ltlCannotRegister.Visible = isValidTime && !mayRegister;
-                this.ltlInvalidTime.Visible = !isValidTime;
+                //this.lnkAvailability.Visible = isValidTime && mayRegister;
+                //this.ltlCannotRegister.Visible = isValidTime && !mayRegister;
+                //this.ltlInvalidTime.Visible = !isValidTime;
+
+                foreach(DataColumn dc in dt.Columns)
+                {
+                    this.ltlNotResident.Text += String.Format("<br />{0}: {1}", dc.ColumnName, dr[dc.ColumnName].ToString());
+                }
             }
             else if (dt != null && dt.Rows.Count == 0)
             {
@@ -163,83 +185,6 @@ namespace Housing
                     "We were unable to retrieve your information. Please contact Nina Fleming <a href='mailto:nfleming@carthage.edu'>nfleming@carthage.edu</a> to resolve this issue."
                 );
                 */
-            }
-        }
-
-        protected void InitScreen()
-        {
-            try
-            {
-                Exception ex = null;
-                string studentSQL = @"
-                    SELECT
-                        id_rec.id, TRIM(id_rec.firstname) AS firstname, TRIM(id_rec.lastname) AS lastname, profile_rec.sex, TRIM(aa_rec.line1) AS email, stu_stat_rec.cum_earn_hrs AS careerhours,
-                        TRIM(stu_serv_rec.bldg) AS bldg, TRIM(stu_serv_rec.room) AS room, stu_acad_rec.reg_hrs AS registered_hours, ADVpay.hld AS advPayHold, UNBal.hld AS unbalHold,
-                        involve_rec.invl AS greekid, invl_table.txt AS greekname, TRIM(upcoming.bldg) AS upcomingBldg, TRIM(upcoming.room) AS upcomingRoom
-                    FROM id_rec	INNER JOIN	profile_rec			        ON	id_rec.id			=	profile_rec.id
-                                LEFT JOIN	aa_rec				        ON	id_rec.id			=	aa_rec.id
-											                            AND	aa_rec.aa			=	'EML1'
-                                LEFT JOIN	stu_stat_rec		        ON	id_rec.id			=	stu_stat_rec.id
-                                LEFT JOIN	stu_serv_rec		        ON	id_rec.id			=	stu_serv_rec.id
-											                            AND	stu_serv_rec.sess	=	@SpringSess
-											                            AND	stu_serv_rec.yr		=	YEAR(TODAY)
-			                    LEFT JOIN	stu_serv_rec	upcoming	ON	id_rec.id	        =	upcoming.id
-											                            AND	upcoming.sess		=	@FallSess
-											                            AND	upcoming.yr			=	YEAR(TODAY)
-			                    LEFT JOIN	stu_acad_rec		        ON	id_rec.id			=	stu_acad_rec.id
-											                            AND	stu_acad_rec.sess	=	@FallSess2
-											                            AND	stu_acad_rec.yr		=	YEAR(TODAY)
-			                    LEFT JOIN	hold_rec	    ADVpay	    ON	id_rec.id			=	ADVpay.id
-											                            AND	TODAY			BETWEEN	ADVpay.beg_date	AND	NVL(ADVpay.end_date, TODAY)
-											                            AND	ADVpay.hld			=	'APAY'
-			                    LEFT JOIN	hold_rec	UNBal	        ON	id_rec.id			=	UNBal.id
-											                            AND	TODAY			BETWEEN	UNBal.beg_date	AND	NVL(UNBal.end_date, TODAY)
-											                            AND	UNBal.hld			=	'UBAL'
-			                    LEFT JOIN	(
-				                    SELECT		invl, id, MAX(beg_date) AS beg_date, MAX(end_date) AS end_date
-				                    FROM		involve_rec
-				                    WHERE		id = @ID
-				                    GROUP BY	invl, id
-			                    )			involve_rec			ON	id_rec.id			=	involve_rec.id
-											                    AND
-											                    (
-												                    NVL(involve_rec.invl,'')	IN	('','S007','S025','S045','S061','S063','S092','S141','S152','S165','S168','S189','S190','S192','S194')
-											                    )
-											                    AND	NVL(involve_rec.beg_date,@InvolveStartDate)	>=	@InvolveStartDate2
-			                    LEFT JOIN	invl_table			ON	involve_rec.invl	=	invl_table.invl
-                    WHERE id_rec.id	=	@ID2
-                ";
-
-                List<OdbcParameter> parameters = new List<OdbcParameter>
-                {
-                    new OdbcParameter("@SpringSess", SpringSession),
-                    new OdbcParameter("@FallSess", FallSession),
-                    new OdbcParameter("@FallSess2", FallSession),
-                    new OdbcParameter("@ID", int.Parse(PortalUser.Current.HostID)),
-                    new OdbcParameter("@InvolveStartDate", BeginInvolvement),
-                    new OdbcParameter("@InvolveStartDate2", BeginInvolvement),
-                    new OdbcParameter("@ID2", int.Parse(PortalUser.Current.HostID))
-                };
-
-                DataTable dt = null;
-                /*
-                dt = odbcConn.ConnectToERP(studentSQL, ref ex, parameters);
-                if (dt.Rows.Count > 1)
-                {
-                    this.errMsg.ErrorMessage = "Too many rows were found. Please check the SQL and verify the results.";
-                }
-                */
-            }
-            catch (Exception ee)
-            {
-                this.ParentPortlet.ShowFeedback(FeedbackType.Error, ee.Message);
-            }
-            finally
-            {
-                if (odbcConn.IsNotClosed())
-                {
-                    odbcConn.Close();
-                }
             }
         }
 
